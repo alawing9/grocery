ab1c3d5acc52dcfd1926e650f9cd63fab44415cd
saasbook <saasbook@saasbook.(none)>
2014-08-25 13:25:45 -0400

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Grocery::Application.config.secret_token = '1f6f080fbb8c9fda5f861d31ab84f5db672b9e519db7d7a4cb83c6198f2848b00f56a450d462dfeb81b40e0e1f1b87707d8211d12f490fe2174216e6c5273441'
